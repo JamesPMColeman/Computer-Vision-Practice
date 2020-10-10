1e876b94659e0fd81e40a2a0b79ec7207eb294f9
James Coleman <jcolem22@msudenver.edu>
2020-10-10 14:09:28 -0600

    Set up a show method to display images and imported my two subject images

--- Homework3/face_detection_by_skin.py
+++ Homework3/face_detection_by_skin.py
@@ -8,19 +8,39 @@
 #===============================================================#
 
     #              >>>>>>>>>> Goals <<<<<<<<<<
-	#
-	#	1. Detect skin in normal and low light images
-	#	2. Remove the background of the low light image
-	#	before doing skin detection
-	#	3. Display results
-	#
-	#
-	#
+    #
+    #   1. Detect skin in normal and low light images
+    #   2. Remove the background of the low light image
+    #   before doing skin detection
+    #   3. Display results
+    #
+    #
+    #
 
 # Imports
+import numpy
+import cv2
+from matplotlib import pyplot
 
 # Helper methods
+def show(image, title):
+    """ Helper method to display a single image 
+    with pyplot """
+    pyplot.figure()
+    pyplot.title(title)
+    pyplot.imshow(image)
+    pyplot.show()
+
 # Read in the images, convert them to RGB and display
+normal_light = cv2.imread('face_good.bmp')
+low_light    = cv2.imread('face_dark.bmp')
+
+normal_light = cv2.cvtColor(normal_light, cv2.COLOR_BGR2RGB)
+low_light    = cv2.cvtColor(low_light, cv2.COLOR_BGR2RGB)
+
+show(normal_light, "Normal Light")
+show(low_light, "Low Light")
+
 # Use a color mask to isolate skin in normal light image
 # Convert low light image to Lumens image, remove background
 # Use lumens image as a mask on lowlight image
