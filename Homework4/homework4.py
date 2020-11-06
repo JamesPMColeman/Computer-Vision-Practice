b20d302c0f14e4d39d93a7a933bad65223e63920
James Coleman <jcolem22@msudenver.edu>
2020-11-06 15:39:37 -0700

    Added PSNR quality measure

--- Homework4/homework4.py
+++ Homework4/homework4.py
@@ -19,8 +19,9 @@
 
 # Imports
 import cv2
+import math
 import numpy
-from random import Random
+import random
 from matplotlib import pyplot
 
 def show(image, title):
@@ -39,12 +40,46 @@ def gaussian_noise(image):
 	return image + noise
 
 def salt_pepper_noise(image):
-	pass
+	""" Creates a salt and pepper noise corruption of image """
+	l, w = image.shape[:2]
+	salt_and_pepper = numpy.ones((l, w))
+	for i in range(l):
+		for j in range(w):
+			pixel = random.randint(0, 15)
+			if (pixel == 0):
+				salt_and_pepper[i][j] = pixel
+			elif (pixel == 1):
+				salt_and_pepper[i][j] = 255
+			else:
+				salt_and_pepper[i][j] = image[i][j]
+	return salt_and_pepper
 
 def contrast_stretch(image):
-	pass
+	""" Creates a contrast stretch enhancement of image """
+	pixel_max = numpy.amax(image)
+	pixel_min = numpy.amin(image)
+	return (image - pixel_min) / (pixel_max - pixel_min) * 255
 
 def gamma_correction(image):
+	""" Creates a contrast stretch enhancement of image """
+	l, w = image.shape[:2]
+	pixel_max = numpy.amax(image)
+	pixel_min = numpy.amin(image)
+	gamma = numpy.zeros((l, w))
+	image_binary = image / 255
+	gamma = image_binary ** 1.8
+	return gamma
+
+def quality_by_PSNR(original, altered):
+	mean_square = numpy.mean((original, altered))
+	if (mean_square == 0): 
+		return('inf')
+	return str(20 * math.log10(255 / math.sqrt(mean_square)))
+
+def quality_by_MSE(original, altered):
+	pass
+
+def quality_by_SSIM(original, altered):
 	pass
 
 def get_original(file_name):
@@ -58,5 +93,14 @@ if __name__ == "__main__":
 	image_file = "lena_g.bmp"
 	original = get_original(image_file)
 	gaussian = gaussian_noise(original)
+	salt_pep = salt_pepper_noise(original)
+	contrast = contrast_stretch(original)
+	gamma_im = gamma_correction(original)
+
+	psnr = quality_by_PSNR(original, gaussian)
+
 	show(original, "Original")
 	show(gaussian, "Gaussian")
+	show(salt_pep, "Salt and Pepper")
+	show(contrast, "Contrast stretch")
+	show(gamma_im, "Gamma correction")
