28d10b9dd8367fccaa14bdce2c4973dda05abf61
James Coleman <jcolem22@msudenver.edu>
2020-11-06 11:50:50 -0700

    I've started a new task to learn how to assess image quality.

---
+++ Homework4/homework4.py
@@ -0,0 +1,35 @@
+#=============================================================================#
+#																			  #
+#			James Coleman													  #
+#			CS 3150															  #
+#			Homework 4														  #
+#			November 6th, 2020												  #
+#																			  #
+#=============================================================================#
+
+		#
+		#		>>>>>>>>>>>>>>>>>>> Goals <<<<<<<<<<<<<<<<<<<<<
+		#
+		#	1. Create two corrupted versions of the Lena image
+		#	2. Create two enhanced versions of the Lena image
+		#	3. Measure each image's quality by PSNR, MSE and SSIM
+		#
+		#
+		#
+
+# Imports
+
+def show(image, title):
+	pass
+
+def gaussian_noise(image):
+	pass
+
+def salt_pepper_noise(image):
+	pass
+
+def contrast_stretch(image):
+	pass
+
+def gamma_correction(image):
+	pass
