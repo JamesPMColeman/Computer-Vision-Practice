bc0c832d275b811a59f135d0a06eea6478945d05
James Coleman <jcolem22@msudenver.edu>
2020-12-10 22:26:16 -0700

    Holds identified and circled
    I have isolated and circled holds by of a specific
    color, using masks and convexhull detection. Next
    step is to make it trivial to choose between diferent
    colors.

--- Bouldering_routs/prj2.py
+++ Bouldering_routs/prj2.py
@@ -18,6 +18,7 @@
 
 # imports
 import cv2
+import math
 import numpy
 from matplotlib import cm
 from matplotlib import pyplot
@@ -31,31 +32,66 @@ def max_of_three(i, j, k):
     return mot if (mot > k) else k
 
 def show(image, color):
-	""" Helper method to display a single image 
-	with pyplot """
-	if (color == "gray"):
-		pyplot.imshow(image, cmap="gray")
-	else:
-		pyplot.imshow(image)
-	pyplot.show()
+    """ Helper method to display a single image 
+    with pyplot """
+    if (color == "gray"):
+        pyplot.imshow(image, cmap="gray")
+    else:
+        pyplot.imshow(image)
+    pyplot.show()
 
 def plot(image, c1, c2, c3, x, y, z):
-	p = pyplot.figure()
-	pixels = image.reshape((numpy.shape(image)[0] * numpy.shape(image)[1], 3))
-	normal = colors.Normalize(vmin=-1.,vmax=1.)
-	normal.autoscale(pixels)
-	pixels = normal(pixels).tolist()
-	axis = p.add_subplot(1, 1, 1, projection="3d")
-	axis.scatter(c1.flatten(), c2.flatten(), c3.flatten(), \
-				 c=pixels, marker=".")
-	axis.set_xlabel(x)
-	axis.set_ylabel(y)
-	axis.set_zlabel(z)
-	pyplot.show()
-	
+    p = pyplot.figure()
+    pixels = image.reshape((numpy.shape(image)[0] * numpy.shape(image)[1], 3))
+    normal = colors.Normalize(vmin=-1.,vmax=1.)
+    normal.autoscale(pixels)
+    pixels = normal(pixels).tolist()
+    axis = p.add_subplot(1, 1, 1, projection="3d")
+    axis.scatter(c1.flatten(), c2.flatten(), c3.flatten(), \
+                 c=pixels, marker=".")
+    axis.set_xlabel(x)
+    axis.set_ylabel(y)
+    axis.set_zlabel(z)
+    pyplot.show()
+
+def sub_matrix(matrix, n):
+    h, w = matrix.shape[:2]
+    temp = numpy.zeros((h, w))
+    for i in range(h):
+        for j in range(w):
+            temp[i,j] = matrix[i,j,n]
+    return temp
+
+    
+def dilate_and_erode(image):
+    """Using openCV method to get rid of extra lines on
+    the image"""
+    blue = sub_matrix(image, 0)
+    green = sub_matrix(image, 1)
+    red = sub_matrix(image, 2)
+
+    kernel2 = numpy.ones((3,3))
+    w, h = image.shape[:2]
+
+    blue = cv2.dilate(blue, kernel2, iterations=2)
+    green = cv2.dilate(green, kernel2, iterations=2)
+    red = cv2.dilate(red, kernel2, iterations=2)
+
+    blue = cv2.erode(blue, kernel2, iterations=2)
+    green = cv2.erode(green, kernel2, iterations=2)
+    red = cv2.erode(red, kernel2, iterations=2)
+
+    for i in range(w):
+        for j in range(h):
+            image[i,j,0] = blue[i,j]
+            image[i,j,1] = green[i,j]
+            image[i,j,2] = red[i,j]
+
+    show(image, "with dilation")
+    return image
 
 # read input image
-wall = cv2.imread('./test4.jpg')
+wall = cv2.imread('./test2.jpg')
 ## show(wall)
 length, width, depth = wall.shape
 
@@ -94,7 +130,8 @@ blue_high = (100, 155, 255)
 blue_low = (25, 45, 120)
 purple_high = (140, 70, 120)
 purple_low = (75, 40, 60)
-
+white_high = (200, 200, 200)
+white_low = (150, 150, 150)
 ## yellow_swatche = numpy.full((10, 10, 3), yellow_high, dtype=numpy.uint8)
 ## gray_swatche = numpy.full((10, 10, 3), yellow_low, dtype=numpy.uint8)
 ## show(yellow_swatche, "RGB")
@@ -112,10 +149,50 @@ blue_mask = cv2.inRange(wall, blue_low, blue_high)
 blue_holds = cv2.bitwise_and(wall, wall, mask=blue_mask)
 purple_mask = cv2.inRange(wall, purple_low, purple_high)
 purple_holds = cv2.bitwise_and(wall, wall, mask=purple_mask)
+white_mask = cv2.inRange(wall, white_low, white_high)
+white_holds = cv2.bitwise_and(wall, wall, mask=white_mask)
 
-show(blue_mask, "gray")
-show(blue_holds, "RGB")
+# show(pink_mask, "gray")
+# show(pink_holds, "RGB")
+box = numpy.ones((5, 5))
+# pink_holds = dilate_and_erode(pink_holds)
+pink_gray = cv2.cvtColor(pink_holds, cv2.COLOR_RGB2GRAY)
+pink_gray = cv2.morphologyEx(pink_gray, cv2.MORPH_OPEN, box)
+show(pink_gray, "gray")
+blurred_holds = cv2.blur(pink_gray, (3, 3))
+# show(blurred_holds, "gray")
+toss, thresh = cv2.threshold(blurred_holds, 50, 255, cv2.THRESH_BINARY)
+# show(thresh, "gray")
+pink_contours, hierarchy = cv2.findContours(thresh, 
+                                            cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+print(len(pink_contours))
 
+for con in pink_contours:
+    c1, c2, c3, c4 = cv2.boundingRect(con)
+
+    (x, y), radius = cv2.minEnclosingCircle(con)
+
+    print(radius)
+    if (radius > 50):
+        for i in range(length):
+            for j in range(width):
+                if math.sqrt((i - y)**2 + (j - x)**2) < radius + 4 and \
+                   math.sqrt((i - y)**2 + (j - x)**2) > radius:
+                    wall[i][j] = 255
+
+show(wall, "RGB")
+'''pink_hull = [] 
+for i in contours:
+    pink_hull.append(cv2.convexHull(i, False))
+
+final_holds = numpy.zeros((tresh.shape[0], thresh.shape[1], 3), numpy.uint8)
+
+for i in range(len(contours)):
+    cv2.drawContours(drawing, contours, i, pink_high, 1, 8, hierarchy)
+    cv2.drawContours(drawing, pink_hull, i, pink_low, 1, 8)
+
+show(pink_holds, "RGB")
 # circle routes of a particular color
-
+'''
 # connect route as a graph
